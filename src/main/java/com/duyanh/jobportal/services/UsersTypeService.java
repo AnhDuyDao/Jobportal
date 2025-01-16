62bce799b10476b96093907a9a0ed5390e1b97a8
Duy Anh <duyanhdao297@gmail.com>
2025-01-16 22:20:03 +0700

    Develop Registratrion

---
+++ src/main/java/com/duyanh/jobportal/services/UsersTypeService.java
@@ -0,0 +1,20 @@
+package com.duyanh.jobportal.services;
+
+import com.duyanh.jobportal.entity.UsersType;
+import com.duyanh.jobportal.repository.UsersTypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersTypeService {
+    private final UsersTypeRepository usersTypeRepository;
+
+    public UsersTypeService(UsersTypeRepository usersTypeRepository) {
+        this.usersTypeRepository = usersTypeRepository;
+    }
+
+    public List<UsersType> getAll() {
+        return usersTypeRepository.findAll();
+    }
+}
