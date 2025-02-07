51798d66776f83cc469dc2e95055dc659a0997b8
Duy Anh <duyanhdao297@gmail.com>
2025-02-07 17:27:39 +0700

    Update Feb 7th

---
+++ src/main/java/com/duyanh/jobportal/entity/IRecruiterJob.java
@@ -0,0 +1,21 @@
+package com.duyanh.jobportal.entity;
+
+public interface IRecruiterJob {
+    Long getTotalCandidates();
+
+    int getJob_post_id();
+
+    String getJob_title();
+
+    int getLocationId();
+
+    String getCity();
+
+    String getState();
+
+    String getCountry();
+
+    int getCompanyId();
+
+    String getName();
+}
