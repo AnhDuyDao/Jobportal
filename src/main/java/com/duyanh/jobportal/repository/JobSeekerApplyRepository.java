8684cef2fa5a88fb61c582704ef227881c741627
Duy Anh <duyanhdao297@gmail.com>
2025-02-10 01:11:05 +0700

    Update Feb 10th 1

---
+++ src/main/java/com/duyanh/jobportal/repository/JobSeekerApplyRepository.java
@@ -0,0 +1,17 @@
+package com.duyanh.jobportal.repository;
+
+import com.duyanh.jobportal.entity.JobPostActivity;
+import com.duyanh.jobportal.entity.JobSeekerApply;
+import com.duyanh.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobSeekerApplyRepository extends JpaRepository<JobSeekerApply, Integer> {
+
+    List<JobSeekerApply> findByUserId(JobSeekerProfile userId);
+
+    List<JobSeekerApply> findByJob(JobPostActivity job);
+}
---
+++ src/main/java/com/duyanh/jobportal/services/JobSeekerApplyService.java
@@ -0,0 +1,28 @@
+package com.duyanh.jobportal.services;
+
+import com.duyanh.jobportal.entity.JobPostActivity;
+import com.duyanh.jobportal.entity.JobSeekerApply;
+import com.duyanh.jobportal.entity.JobSeekerProfile;
+import com.duyanh.jobportal.repository.JobSeekerApplyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerApplyService {
+
+    private final JobSeekerApplyRepository jobSeekerApplyRepository;
+    @Autowired
+    public JobSeekerApplyService(JobSeekerApplyRepository jobSeekerApplyRepository) {
+        this.jobSeekerApplyRepository = jobSeekerApplyRepository;
+    }
+
+    public List<JobSeekerApply> getCandidatesJobs(JobSeekerProfile userAccountId) {
+        return jobSeekerApplyRepository.findByUserId(userAccountId);
+    }
+
+    public List<JobSeekerApply> getJobCandidates(JobPostActivity job) {
+        return jobSeekerApplyRepository.findByJob(job);
+    }
+}
