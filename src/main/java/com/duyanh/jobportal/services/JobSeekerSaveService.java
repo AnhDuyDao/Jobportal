116ba9d7a67b089e50b04c88b1ef3c3f171ed40f
Duy Anh <duyanhdao297@gmail.com>
2025-02-13 01:13:26 +0700

    Jobseeker apply

---
+++ src/main/java/com/duyanh/jobportal/services/JobSeekerSaveService.java
@@ -0,0 +1,26 @@
+package com.duyanh.jobportal.services;
+
+import com.duyanh.jobportal.entity.JobPostActivity;
+import com.duyanh.jobportal.entity.JobSeekerProfile;
+import com.duyanh.jobportal.entity.JobSeekerSave;
+import com.duyanh.jobportal.repository.JobSeekerSaveRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerSaveService {
+    private final JobSeekerSaveRepository jobSeekerSaveRepository;
+
+    public JobSeekerSaveService(JobSeekerSaveRepository jobSeekerSaveRepository) {
+        this.jobSeekerSaveRepository = jobSeekerSaveRepository;
+    }
+
+    public List<JobSeekerSave> getCandidatesJob(JobSeekerProfile userAccountId) {
+        return jobSeekerSaveRepository.findByUserId(userAccountId);
+    }
+
+    public List<JobSeekerSave> getJobCandidates(JobPostActivity job) {
+        return jobSeekerSaveRepository.findByJob(job);
+    }
+}
--- src/main/java/com/duyanh/jobportal/services/RecruiterProfileService.java
+++ src/main/java/com/duyanh/jobportal/services/RecruiterProfileService.java
@@ -1,7 +1,13 @@
 package com.duyanh.jobportal.services;
 
 import com.duyanh.jobportal.entity.RecruiterProfile;
+import com.duyanh.jobportal.entity.Users;
 import com.duyanh.jobportal.repository.RecruiterProfileRepository;
+import com.duyanh.jobportal.repository.UsersRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,10 +15,12 @@ import java.util.Optional;
 @Service
 public class RecruiterProfileService {
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final UsersRepository usersRepository;
 
 
-    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository, UsersRepository usersRepository) {
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Optional<RecruiterProfile> getOne(Integer id) {
@@ -21,5 +29,15 @@ public class RecruiterProfileService {
 
     public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
         return recruiterProfileRepository.save(recruiterProfile);
+    }
+
+    public RecruiterProfile getCurrentRecruiterProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Optional<RecruiterProfile> recruiterProfile = getOne(users.getUserId());
+            return recruiterProfile.orElse(null);
+        } else return null;
     }
 }
